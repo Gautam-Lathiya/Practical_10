a017885b949f28ede03ab21d9ccb412798978b12
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-05 22:07:15 +0530

    Implement functionalities for Task-4

---
+++ Test4/App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Test4.Filters;
+
+namespace Test4
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new CustomExceptionFilter());
+        }
+    }
+}
---
+++ Test4/Controllers/ExceptionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test4.Filters;
+
+namespace Test4.Controllers
+{
+    [CustomExceptionFilter]
+    public class ExceptionController : Controller
+    {
+        // GET: Exception
+        public ActionResult Index()
+        {
+            int a = 0;
+            int result = 1 / a; 
+            return View();
+        }
+    }
+}
