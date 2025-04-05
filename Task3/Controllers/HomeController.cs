448ce2b46be2c98ae89baebe512f7868825cd386
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-05 19:05:26 +0530

    Implement functionalities for Task-3

---
+++ Task3/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [OutputCache(Duration = 300)] // 5 minutes = 300 seconds
+        public ActionResult Index()
+        {
+            string currentTime = DateTime.Now.ToString();
+            return Content("Cached Time: " + currentTime);
+        }
+
+    }
+}
